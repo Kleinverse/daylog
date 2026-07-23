a0abe0b250a5e5203297b4fab964305fe447763c
administrator <administrator@kleinverse.io>
2026-07-23 07:45:50 +0000

    revise document

--- docs/js/index.js
+++ docs/js/index.js
@@ -110,7 +110,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const register = document.getElementById("_register");
     if(register){
         register.addEventListener('click', () => {
-            console.log('clicked');
             const resuUrl = document.getElementById("_restUrl");
             const agree = document.getElementById("_agree");
             if(restUrl.value.trim() == "") restUrl.classList.add("uk-form-danger");
